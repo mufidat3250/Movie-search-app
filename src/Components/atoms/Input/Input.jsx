d6f96fc15834bf5a9d954e4c5252870d78f00c28
mufidat3250 <wahabmufidat1919@gmail.com>
2022-12-09 15:55:00 +0100

    fixes

--- src/Components/atoms/Input/Input.jsx
+++ src/Components/atoms/Input/Input.jsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 export const InputWrapper = styled.div`
     width: 100%;
     input{
-        outline: none;
-        width: 100%;
+    outline: none;
+    width: 100%;
     border: 1px solid black;
     height: 4.8125rem;
     font-size: 1.5rem;
     margin-top: 4px;
+    padding-left:1rem;
     }
     label{
         font-size:1.25rem;
